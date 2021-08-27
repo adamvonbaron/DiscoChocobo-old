e88da3c9b7284032cd8704aba361a7fb3119348f
adam <apk@hey.com>
2021-08-26 21:36:24 -0400

    use cool new python way

--- utils.py
+++ utils.py
@@ -3,4 +3,4 @@ from urllib import parse
 
 def validate_youtube_url(url: str):
     parse_result = parse.urlparse(url)
-    return "youtube.com/watch" in parse_result.path
+    return "youtube.com/watch" in parse_result.geturl()
